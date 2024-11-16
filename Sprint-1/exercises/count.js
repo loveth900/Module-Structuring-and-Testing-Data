dee72712ae237d743decca1f3e8818ef7e6395e7
loveth900 <ojay_love@yahoo.com>
2024-11-16 15:52:50 +0000

    made a change by adding 0 to 1

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -1,6 +1,7 @@
 let count = 0;
 
 count = count + 1;
-
+console.log(count)
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+// line is adding 0 to 1 which is 1
