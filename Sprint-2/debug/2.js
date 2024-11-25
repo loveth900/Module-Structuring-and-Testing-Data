e1bd38e38328df1e312f4cf63f7ba87bc18a4d35
loveth900 <ojay_love@yahoo.com>
2024-11-25 18:00:41 +0000

    the log is always three

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
+// Prediction : num is defined with const. All of these console.log()s will return to"3".
 
-const num = 103;
+let num = 103;
 
 function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -12,3 +14,5 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+//I had to make a parameter for the function to take in instead of having a constant value that it uses, I made the const let and I added the parameter num to the getLast Digit Function.
