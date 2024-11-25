071f3760082f6f43fe9e2965065b4646dba7ce21
loveth900 <ojay_love@yahoo.com>
2024-11-25 16:51:24 +0000

    the sum is null

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+// Prediction : it will return "the sum of 10 and 32 is null". Because the function returns something but it's null.
 
 function sum(a, b) {
   return;
@@ -6,3 +7,4 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// The result of summing 10 and 32 is 320
