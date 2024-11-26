05b26acd95dffdc82abdf87d3ca2e321acc007bd
loveth900 <ojay_love@yahoo.com>
2024-11-26 17:32:37 +0000

    i have fixed the error

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
+// The provided code contains a fundamental error in the way variables are declared
 
 // Why will an error occur when this program runs?
+
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
 
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5));  // Output: "50%"
+console.log(convertToPercentage(0.25)); // Output: "25%"
+console.log(convertToPercentage(1));    // Output: "100%"
+
