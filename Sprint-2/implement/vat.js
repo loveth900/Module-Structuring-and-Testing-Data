d1e98415babea4315c355689e9805e3a206e2523
loveth900 <ojay_love@yahoo.com>
2024-11-27 03:29:57 +0000

    i implemented the function and calculated the new price

--- Sprint-2/implement/vat.js
+++ Sprint-2/implement/vat.js
@@ -1,10 +1,13 @@
-// If products or services are subject to the 20% standard rate of VAT:
-// You must add 20% to the price you charge for the goods or service.
-// You can do this by multiplying the price you charge by 1.2.
-// For example, if your business sells sports equipment for £50, you multiply £50 by 1.2 for a total VAT inclusive price of £60.
 
-// Implement a function that calculates the new price with VAT (Value Added Tax) added on
+function calculatePriceWithVAT(price) {
+    // Multiply the price by 1.2 to add 20% VAT
+    const priceWithVAT = price * 1.2;
 
-// Given a number,
-// When I call this function with a number
-// it returns the new price with VAT added on
+    // Return the final price
+    return priceWithVAT.toFixed(2); // Round to 2 decimal places for currency
+}
+
+console.log(calculatePriceWithVAT(50));  // Output: 60.00
+console.log(calculatePriceWithVAT(100)); // Output: 120.00
+console.log(calculatePriceWithVAT(29.99)); // Output: 35.99
+console.log(calculatePriceWithVAT(0));   // Output: 0.00
