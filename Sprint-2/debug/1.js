86371c15e6e3995aef86e232e4ce767d6a0a71aa
loveth900 <ojay_love@yahoo.com>
2024-12-10 13:54:41 +0000

    done with prediction

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -8,3 +8,12 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 // The result of summing 10 and 32 is 320
+
+// Predict and explain first...
+
+function sum(a, b) {
+  return;
+  a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
