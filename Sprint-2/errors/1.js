dcd0dc63878866b449c7974d87c8b25ec0d5c1e9
loveth900 <ojay_love@yahoo.com>
2024-12-10 14:16:31 +0000

    redeclaration of decimalNumber

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,8 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(convertToPercentage(0.5)); // Outputs "50%"
