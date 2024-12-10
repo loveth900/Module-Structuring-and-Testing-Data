2633c5179d31c605376483307796bf7aff1975c2
loveth900 <ojay_love@yahoo.com>
2024-12-10 14:19:33 +0000

    function square(3)

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,8 @@
 
-// Predict and explain first...
-
-// this function should square any number but instead we're going to get an error
-
-function square(3) {
+function square(num) {
     return num * num;
 }
+
+console.log(square(3)); // Outputs: 9
 
 
