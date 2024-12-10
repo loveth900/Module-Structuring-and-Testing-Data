6e096049822a7ef4c522f85af4297eaa6b770250
loveth900 <ojay_love@yahoo.com>
2024-12-10 14:12:27 +0000

    error because the variable str

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -3,7 +3,8 @@
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
+
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`; // No `let`, just reassign
   return str;
 }
