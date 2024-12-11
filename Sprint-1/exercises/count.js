9c5edc53f298ad0307c3d51848343a79301a9e79
loveth900 <ojay_love@yahoo.com>
2024-12-11 05:43:50 +0000

    variable declaration was made

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -5,3 +5,6 @@ console.log(count)
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 // line is adding 0 to 1 which is 1
+
+
+console.log(count); // Line 4: Output the value of count
