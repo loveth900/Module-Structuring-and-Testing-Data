223c952fa0071fe944bc82ae6bbee827b91195ac
loveth900 <ojay_love@yahoo.com>
2024-12-11 05:57:53 +0000

    the varible was declared

--- Sprint-1/exercises/random.js
+++ Sprint-1/exercises/random.js
@@ -8,4 +8,4 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
 // Log num
-console.log(num);
+console.log(num); 
