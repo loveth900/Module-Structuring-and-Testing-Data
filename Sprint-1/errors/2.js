1ab1e3cedeefe0c5eb02b8653bc7c9cc32fa806e
loveth900 <ojay_love@yahoo.com>
2024-12-11 05:22:24 +0000

    the varibles was changed to i was born in bolton

--- Sprint-1/errors/2.js
+++ Sprint-1/errors/2.js
@@ -1,7 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 const cityOfBirth = "Bolton";
+ 
+ 
 
 
-console.log(`I was born in ${cityOfBirth}`);
-
+console.log("I was born in " + cityOfBirth);
