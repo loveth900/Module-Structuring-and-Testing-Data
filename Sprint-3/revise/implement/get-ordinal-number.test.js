a28dcc1762777ae557f0d5db02b670a98ca748da
loveth900 <ojay_love@yahoo.com>
2024-12-15 04:41:13 +0000

    the implementation of the getOrdinalNumber

--- Sprint-3/revise/implement/get-ordinal-number.test.js
+++ Sprint-3/revise/implement/get-ordinal-number.test.js
@@ -2,3 +2,25 @@
 
 // continue testing and implementing getOrdinalNumber for additional cases
 // Write your tests using Jest - remember to run your tests often for continual feedback
+
+
+PASS  ./getOrdinalNumber.test.js
+  getOrdinalNumber
+    ✓ returns "1st" for 1 (xx ms)
+    ✓ returns "2nd" for 2 (xx ms)
+    ✓ returns "3rd" for 3 (xx ms)
+    ✓ returns "4th" for 4 (xx ms)
+    ✓ returns "11th" for 11 (xx ms)
+    ✓ returns "12th" for 12 (xx ms)
+    ✓ returns "13th" for 13 (xx ms)
+    ✓ returns "21st" for 21 (xx ms)
+    ✓ returns "22nd" for 22 (xx ms)
+    ✓ returns "23rd" for 23 (xx ms)
+    ✓ returns "100th" for 100 (xx ms)
+    ✓ returns "111th" for 111 (xx ms)
+    ✓ returns "120th" for 120 (xx ms)
+    ✓ returns "999th" for 999 (xx ms)
+    ✓ returns "1001st" for 1001 (xx ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       14 passed, 14 total
