0eccb73768e376f75a24f2279ac70d5c78c8eec9
loveth900 <ojay_love@yahoo.com>
2024-12-15 04:46:36 +0000

    done with prime

--- Sprint-3/revise/implement/is-prime.test.js
+++ Sprint-3/revise/implement/is-prime.test.js
@@ -1,3 +1,24 @@
 // Given a positive integer num,
 // When the isPrime function is called with num as input,
 // Then it should check if the num is prime
+
+
+function isPrime(num) {
+    // Handle edge cases for numbers less than or equal to 1
+    if (num <= 1) return false;
+    
+    // 2 is the only even prime number
+    if (num === 2) return true;
+    
+    // Eliminate even numbers greater than 2
+    if (num % 2 === 0) return false;
+
+    // Check for divisibility from 3 to sqrt(num)
+    for (let i = 3; i * i <= num; i += 2) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+}
+
+module.exports = isPrime;
